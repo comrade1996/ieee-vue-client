dbac332f220186f004bbd1f4b5558b5a81c8789f
khatabwedaa <khatabwedaa@gmail.com>
2020-09-05 14:29:52 +0200

    Add image slider

--- src/components/Events.vue
+++ src/components/Events.vue
@@ -8,63 +8,22 @@
       :autoplayHoverPause="true"
     >
       <slide
-        class="flex px-1"
+        class="flex px-4"
         v-for="(volunteer, index) in volunteers"
         :key="index"
         style="width:fit-content"
       >
         <div class="max-w-sm my-4 overflow-hidden bg-white rounded-lg shadow-lg">
-          <swiper class="swiper" :options="swiperOption">
-            <swiper-slide>
+          <carousel :paginationEnabled="false" :autoplayTimeout="3000" :perPage="1" :autoplay="true" :autoplayHoverPause="true">
+            <slide style="width:20rem;" v-for="(image, index) in volunteer.images" :key="index">
             <img
               class="object-cover object-center w-full h-56"
               loading="lazy"
-              src="../assets/img/volunteers/Mayada_jalal_mohamed_sharif.jpg"
+              :src="image.url"
               alt="avatar"
             />
-            </swiper-slide>
-            <swiper-slide>
-            <img
-              class="object-cover object-center w-full h-56"
-              loading="lazy"
-              :src="volunteer.img"
-              alt="avatar"
-            />
-            </swiper-slide>
-              <swiper-slide>
-            <img
-              class="object-cover object-center w-full h-56"
-              loading="lazy"
-              src="../assets/img/volunteers/Mayada_jalal_mohamed_sharif.jpg"
-              alt="avatar"
-            />
-            </swiper-slide>
-            <div class="swiper-button-prev" slot="button-prev"></div>
-            <div class="swiper-button-next" slot="button-next"></div>
-          </swiper>
-
-          <!-- <agile :dots="true" :infinite="true" :navButtons="true">
-            <div  class="w-full !important" style="width: 100% !important;">
-              <img
-                class="object-cover object-center w-full h-56"
-                loading="lazy"
-                :src="volunteer.img"
-                alt="avatar"
-              />
-            </div>
-
-            <div class="w-full" style="width: 100% !important;">
-              <img
-                class="object-cover object-center w-full h-56"
-                loading="lazy"
-                :src="volunteer.img"
-                alt="avatar"
-              />
-            </div>
-
-            <template slot="prevButton">prev</template>
-            <template slot="nextButton">next</template>
-          </agile>-->
+            </slide>
+          </carousel>
           <div class="flex items-center px-6 py-3 bg-gray-900">
             <svg class="w-6 h-6 text-white fill-current" viewBox="0 0 512 512">
               <path
@@ -81,12 +40,6 @@
           <div class="px-6 py-4">
             <h1 class="text-2xl font-semibold text-gray-800">{{volunteer.name}}</h1>
             <p class="py-2 text-lg text-gray-700">{{volunteer.job}}</p>
-            <!-- <div class="flex items-center mt-4 text-gray-700">
-                <svg class="w-6 h-6 fill-current" viewBox="0 0 512 512">
-                    <path d="M239.208 343.937c-17.78 10.103-38.342 15.876-60.255 15.876-21.909 0-42.467-5.771-60.246-15.87C71.544 358.331 42.643 406 32 448h293.912c-10.639-42-39.537-89.683-86.704-104.063zM178.953 120.035c-58.479 0-105.886 47.394-105.886 105.858 0 58.464 47.407 105.857 105.886 105.857s105.886-47.394 105.886-105.857c0-58.464-47.408-105.858-105.886-105.858zm0 186.488c-33.671 0-62.445-22.513-73.997-50.523H252.95c-11.554 28.011-40.326 50.523-73.997 50.523z"/><g><path d="M322.602 384H480c-10.638-42-39.537-81.691-86.703-96.072-17.781 10.104-38.343 15.873-60.256 15.873-14.823 0-29.024-2.654-42.168-7.49-7.445 12.47-16.927 25.592-27.974 34.906C289.245 341.354 309.146 364 322.602 384zM306.545 200h100.493c-11.554 28-40.327 50.293-73.997 50.293-8.875 0-17.404-1.692-25.375-4.51a128.411 128.411 0 0 1-6.52 25.118c10.066 3.174 20.779 4.862 31.895 4.862 58.479 0 105.886-47.41 105.886-105.872 0-58.465-47.407-105.866-105.886-105.866-37.49 0-70.427 19.703-89.243 49.09C275.607 131.383 298.961 163 306.545 200z"/></g>
-                </svg>
-                <h1 class="px-2 text-sm">{{volunteer.team}}</h1>
-            </div>-->
             <div class="flex items-center mt-4 text-gray-700">
               <svg class="w-6 h-6 fill-current" viewBox="0 0 512 512">
                 <path
@@ -105,15 +58,6 @@
             </div>
           </div>
         </div>
-
-        <!-- <pre class="flex-col float-right"  style="transform: rotate(270deg); background:grey;  height: fit-content;">{{volunteer.team}}</pre>
-
-<div class="flex-col">
-        <img :src="volunteer.img" class="object-cover w-full h-56 rounded-md shadow-md " />
-        <h2 class="mt-4 text-xl font-semibold text-gray-800">{{volunteer.name}}</h2>
-        <h2 class="mt-4 text-xl font-semibold text-gray-800">{{volunteer.team}}</h2>
-        <h2 class="mt-4 text-xl font-semibold text-gray-800">{{volunteer.job}}</h2>
-        </div>-->
       </slide>
     </carousel>
     <div class="flex items-center justify-center mt-8">
@@ -145,267 +89,63 @@ export default {
       volunteers: [
         {
           name: "Abdalaziz Mousa alrofai Mohammed",
-          img: require("../assets/img/volunteers/Abdalaziz_Mousa_alrofai_Mohammed.jpg"),
+          images: [
+            { url: require("../assets/img/volunteers/Abdalaziz_Mousa_alrofai_Mohammed.jpg") },
+            { url: "https://images.unsplash.com/photo-1599229752248-419f05f01faa?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=751&q=80" },
+            { url: "https://images.unsplash.com/photo-1599058919120-9ed1015f0fdf?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=334&q=80" },
+            { url: "https://images.unsplash.com/photo-1599058919120-9ed1015f0fdf?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=334&q=80" },
+            { url: "https://images.unsplash.com/photo-1599218422795-545e7e2f5eae?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=334&q=80" }
+          ],
           team: "Multimedia",
           job: "Graphics Designer - video editor",
         },
         {
-          name: "Banan Babiker Osman Babiker",
-          img: require("../assets/img/volunteers/Banan_Babiker_Osman_Babiker.jpg"),
-          team: "Financial",
-          job: "Head of Financial",
-        },
-        {
-          name: "Mayada jalal mohamed sharif",
-          img: require("../assets/img/volunteers/Mayada_jalal_mohamed_sharif.jpg"),
-          team: "Marketing",
-          job: "Member",
-        },
-        {
-          name: "Mazin Hafiz Eltahir Mohamed",
-          img: require("../assets/img/volunteers/Mazin_Hafiz_Eltahir_Mohamed.jpg"),
-          team: "Program management",
-          job: "Head of program management",
-        },
-        {
-          name: "Mozan Fateh Elaleem mussa Abdullah",
-          img: require("../assets/img/volunteers/Mozan_Fateh_Elaleem_mussa_Abdullah.jpg"),
-          team: "programming management and public relationships ",
-          job: "translater and text editor",
-        },
-        {
-          name: "Maaz salah Ahmed Mohamed",
-          img: require("../assets/img/volunteers/Maaz_salah_Ahmed_Mohamed.jpg"),
-          team: "strategic planning",
-          job: "Head of strategic planning",
-        },
-        {
-          name: "salwa Hafiz Sulieman mohammed ahmed",
-          img: require("../assets/img/volunteers/salwa_Hafiz_Sulieman_mohammed_Ahmed.jpg"),
-          team: "stratigic palnning",
-          job: "Member",
-        },
-        {
-          name: "Samar Abd Algadir ahmed mustafa",
-          img: require("../assets/img/volunteers/Samar_Abd_Algadir_ahmed_mustafa.jpg"),
-          team: "Puplic realations",
-          job: "Member",
-        },
-        {
-          name: "Abrar Elzobier Eltayeb Mohammed",
-          img: require("../assets/img/volunteers/Abrar_Elzobier_Eltayeb_Mohammed.jpg"),
-          team: "Public relations",
-          job: "Member",
-        },
-        {
-          name: "Muzzamil Ali Hassan Mahmoud",
-          img: require("../assets/img/volunteers/Muzzamil_Ali_Hassan_Mahmoud.jpg"),
-          team: "logistic",
-          job: "head of logistic",
-        },
-        {
-          name: "alfadil banaga musa Elshiekh",
-          img: require("../assets/img/volunteers/alfadil_banaga_musa_Elshiekh.jpg"),
-          team: "public relations",
-          job: "arrange and organize speeches for events",
-        },
-        {
-          name: "Amaal Mubark Eisa Aldein",
-          img: require("../assets/img/volunteers/Amaal_Mubark_Eisa_Aldein.jpg"),
-          team: "Public relations",
-          job: "Member",
-        },
-        {
-          name: "Albara mohammed abd allah hossain",
-          img: require("../assets/img/volunteers/Albara_mohammed_abd_allah_hossain.jpg"),
-          team: "Financial",
-          job: "Investment",
-        },
-        {
-          name: "AML MOHAMMED HASSAN OSMAN",
-          img: require("../assets/img/volunteers/AML_MOHAMMED_HASSAN_OSMAN.jpg"),
-          team: "Logistics",
-          job: "Member",
-        },
-        {
-          name: "Merghani Mohammed Tayeb Al-asmaa Esmail",
-          img: require("../assets/img/volunteers/Merghani_Mohammed_Tayeb_Al-asmaa_Esmail Makie.jpg"),
-          team: "Strategic",
-          job: "Vice",
-        },
-        {
-          name: "Hawaa Basheer Ali Elamin",
-          img: require("../assets/img/volunteers/Hawaa_Basheer_Ali_Elamin.jpg"),
-          team: "Public relastions",
-          job: "Public relation officer",
-        },
-        {
-          name: "Shadin Yassir",
-          img: require("../assets/img/volunteers/Shadin_Yassir.jpg"),
-          team: "",
-          job: "Vice chairperson",
-        },
-        {
-          name: "Tasabeeh alshafie abdala seed aldeen",
-          img: require("../assets/img/volunteers/Tasabeeh_alshafie.jpg"),
-          team: "Logistic",
-          job: "Technical support",
-        },
-        {
-          name: "Abdallah kamal aldeen ibrahim mohamed",
-          img: require("../assets/img/volunteers/Abdallah_kamal_aldeen_ibrahim_mohamed.jpg"),
+          name: "Abdalaziz Mousa alrofai Mohammed",
+          images: [
+            { url: require("../assets/img/volunteers/Abdalaziz_Mousa_alrofai_Mohammed.jpg") },
+            { url: "https://images.unsplash.com/photo-1599229752248-419f05f01faa?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=751&q=80" },
+            { url: "https://images.unsplash.com/photo-1599058919120-9ed1015f0fdf?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=334&q=80" },
+            { url: "https://images.unsplash.com/photo-1599058919120-9ed1015f0fdf?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=334&q=80" },
+            { url: "https://images.unsplash.com/photo-1599218422795-545e7e2f5eae?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=334&q=80" }
+          ],
           team: "Multimedia",
-          job: "Graphic designer",
+          job: "Graphics Designer - video editor",
         },
         {
-          name: "Malaz Abdalla mohmmed Ahmad",
-          img: require("../assets/img/volunteers/Malaz_Abdalla_mohmmed_Ahmad.jpg"),
-          team: "Public Relations",
-          job: "Member",
-        },
-        {
-          name: "Hassan kamaleldin hassan abdelgader",
-          img: require("../assets/img/volunteers/Hassan_kamaleldin_hassan_abdelgader.jpg"),
-          team: "Multimedia - volunteers Coordinators",
-          job: "Graphics designer",
-        },
-        {
-          name: "Malaz Abdelkhalig sarageldin yousif",
-          img: require("../assets/img/volunteers/Malaz_Abdelkhalig_sarageldin_yousif.jpg"),
-          team: "Marketing",
-          job: "Member",
-        },
-        {
-          name: "Mustafa Hassan Abdelgadir Ali",
-          img: require("../assets/img/volunteers/Mustafa_Hassan_Abdelgadir_Ali.jpg"),
-          team: "Program management",
-          job: "Member",
-        },
-        {
-          name: "Rabaa Mohammed Osman Alfhal",
-          img: require("../assets/img/volunteers/Rabaa_Mohammed_Osman_Alfhal.jpg"),
-          team: "public Relations",
-          job: "Member",
-        },
-        {
-          name: "Suha Kamal Abuzeid Mohamed",
-          img: require("../assets/img/volunteers/Suha_Kamal_Abuzeid_Mohamed.jpg"),
-          team: "Public relations",
-          job: "Vice-Public Relations",
-        },
-        {
-          name: "Rania Fathalrahman Abdelmahamoud Ali",
-          img: require("../assets/img/volunteers/Rania_Fathalrahman_Abdelmahamoud_Ali.jpg"),
-          team: "Marketing",
-          job: "Content writing",
-        },
-        {
-          name: "Fatima Mirghani Ahmed El-faki",
-          img: require("../assets/img/volunteers/Fatima_Mirghani_Ahmed_El-faki.jpg"),
-          team: "volunteers Coordinators",
-          job: "Head of volunteers Coordinators",
-        },
-        {
-          name: "fatima abdalla alageb abdalla ",
-          img: require("../assets/img/volunteers/fatima_abdalla_alageb_abdalla.jpg"),
-          team: "Marketing",
-          job: "Content writer",
-        },
-        {
-          name: "Kareema Osman mohe alden alfke",
-          img: require("../assets/img/volunteers/Kareema_Osman_mohe_alden_alfke.jpg"),
-          team: "Financial",
-          job: "Member",
-        },
-        {
-          name: "Mudther Atif Siddig ElRayah",
-          img: require("../assets/img/volunteers/Mudther_Atif_Siddig_ElRayah.jpg"),
-          team: "Programming management - Software development",
-          job: "Member",
-        },
-        {
-          name: "Mujtaba Salah EL-Din Siddig",
-          img: require("../assets/img/volunteers/Mujtaba_Salah_EL-Din_Siddig.jpg"),
+          name: "Abdalaziz Mousa alrofai Mohammed",
+          images: [
+            { url: require("../assets/img/volunteers/Abdalaziz_Mousa_alrofai_Mohammed.jpg") },
+            { url: "https://images.unsplash.com/photo-1599229752248-419f05f01faa?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=751&q=80" },
+            { url: "https://images.unsplash.com/photo-1599058919120-9ed1015f0fdf?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=334&q=80" },
+            { url: "https://images.unsplash.com/photo-1599058919120-9ed1015f0fdf?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=334&q=80" },
+            { url: "https://images.unsplash.com/photo-1599218422795-545e7e2f5eae?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=334&q=80" }
+          ],
           team: "Multimedia",
-          job: "Member",
+          job: "Graphics Designer - video editor",
         },
         {
-          name: "Mohammed ombadah hamad elbashir",
-          img: require("../assets/img/volunteers/Mohammed_ombadah_hamad_elbashir.jpg"),
+          name: "Abdalaziz Mousa alrofai Mohammed",
+          images: [
+            { url: require("../assets/img/volunteers/Abdalaziz_Mousa_alrofai_Mohammed.jpg") },
+            { url: "https://images.unsplash.com/photo-1599229752248-419f05f01faa?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=751&q=80" },
+            { url: "https://images.unsplash.com/photo-1599058919120-9ed1015f0fdf?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=334&q=80" },
+            { url: "https://images.unsplash.com/photo-1599058919120-9ed1015f0fdf?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=334&q=80" },
+            { url: "https://images.unsplash.com/photo-1599218422795-545e7e2f5eae?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=334&q=80" }
+          ],
           team: "Multimedia",
-          job: "Photographer",
+          job: "Graphics Designer - video editor",
         },
         {
-          name: "Omar Abdulrahman Ali Hassan",
-          img: require("../assets/img/volunteers/Omar_Abdulrahman_Ali_Hassan.jpg"),
+          name: "Abdalaziz Mousa alrofai Mohammed",
+          images: [
+            { url: require("../assets/img/volunteers/Abdalaziz_Mousa_alrofai_Mohammed.jpg") },
+            { url: "https://images.unsplash.com/photo-1599229752248-419f05f01faa?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=751&q=80" },
+            { url: "https://images.unsplash.com/photo-1599058919120-9ed1015f0fdf?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=334&q=80" },
+            { url: "https://images.unsplash.com/photo-1599058919120-9ed1015f0fdf?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=334&q=80" },
+            { url: "https://images.unsplash.com/photo-1599218422795-545e7e2f5eae?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=334&q=80" }
+          ],
           team: "Multimedia",
-          job: "Photographer",
-        },
-        {
-          name: "Rayan Ibrahim Saeed Ibrahim",
-          img: require("../assets/img/volunteers/Rayan_Ibrahim_Saeed_Ibrahim.jpg"),
-          team: "Marketing",
-          job: "Head of Marketing",
-        },
-        {
-          name: "Mohmmad alhashim mojahed abdalla",
-          img: require("../assets/img/volunteers/Mohmmad_alhashim_mojahed_abdalla.jpg"),
-          team: "Public relations",
-          job: "Member",
-        },
-        {
-          name: "Mohammed Saeed Abdualshafi Mohammed",
-          img: require("../assets/img/volunteers/Mohammed_Saeed_Abdualshafi_Mohammed.jpg"),
-          team: "Programming Management",
-          job: "Vice",
-        },
-        {
-          name: "Abdallah Mahmoud Azmi Mahmoud",
-          img: require("../assets/img/volunteers/Abdallah_Mahmoud_Azmi_Mahmoud.jpg"),
-          team: "Multimedia",
-          job: "Head of Multimedia",
-        },
-        {
-          name: "Anas mohamed Osman mohamed ali",
-          img: require("../assets/img/volunteers/Anas_mohamed_Osman_mohamed_ali.jpg"),
-          team: "Logistics",
-          job: "Member",
-        },
-        {
-          name: "Rayan abdalazeem ahmed babikir",
-          img: require("../assets/img/volunteers/Rayan_abdalazeem_ahmed_babikir.jpeg"),
-          team: "Logistics",
-          job: "Secreatry",
-        },
-        {
-          name: "Esra Elshik Alhalfaya bahari",
-          img: require("../assets/img/volunteers/Esra_Elshik_Alhalfaya_bahari.jpeg"),
-          team: "Logistics - volunteers Coordinators",
-          job: "volunteers Coordinators",
-        },
-        {
-          name: "Yousra Alrasheed Mohammed Ahmed",
-          img: require("../assets/img/volunteers/Yousra_Alrasheed_Mohammed_Ahmed.jpeg"),
-          team: "Logistics",
-          job: "Member",
-        },
-        {
-          name: "Mohammed edris musa edris",
-          img: require("../assets/img/volunteers/Mohammed_edris_musa_edris.jpg"),
-          team: "Software Devolopers",
-          job: "Member",
-        },
-        {
-          name: "Omair Gibreel abdallatief ahmed",
-          img: require("../assets/img/volunteers/Omair_Gibreel_abdallatief_ahmed.jpeg"),
-          team: "Software Devolopers",
-          job: "Web master",
-        },
-        {
-          name: "Rayan abdelrahem ali mohammed",
-          img: require("../assets/img/volunteers/Rayan_Abdelrahem_Ali_Mohammed.jpeg"),
-          team: "Public relations",
-          job: "Share and study ideas",
+          job: "Graphics Designer - video editor",
         },
       ],
     };
